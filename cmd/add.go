278899b4d0baf5b495bd4885727ce80847220deb
Adit0507 <adi05072002@gmail.com>
2024-12-31 10:57:36 +0530

    using viper for config management

--- cmd/add.go
+++ cmd/add.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Adit0507/pScan.com/scan"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // addCmd represents the add command
@@ -20,10 +21,8 @@ var addCmd = &cobra.Command{
 	Args: cobra.MinimumNArgs(1),
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		hostsFile, err := cmd.Flags().GetString("hosts-file")
-		if err != nil {
-			return err
-		}
+		hostsFile := viper.GetString("hosts-file")
+
 		return addAction(os.Stdout, hostsFile, args)
 	},
 }
